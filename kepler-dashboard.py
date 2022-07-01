5c20eca6d8a417617df800bab6a39b48573b310a
graeme <graeme.t.benson@gmail.com>
2022-07-01 10:47:13 -0400

    Update kepler-dashboard.py

--- kepler-dashboard.py
+++ kepler-dashboard.py
@@ -8,9 +8,6 @@ Created on Thu Jun 30 12:19:15 2022
 """
 
 # Import necessary packages
-import astroquery
-import astropy
-import numpy as np
 import pandas as pd
 import altair as alt
 import streamlit as st
